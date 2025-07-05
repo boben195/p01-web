33de2d4e57cb9af7655598228e8b86592ed92799
boben195 <boben195@ukr.net>
2025-07-05 16:04:52 +0300

    add data

---
+++ src/Components/data.jsx
@@ -0,0 +1,142 @@
+import { FaCentos } from "react-icons/fa";
+import { PiWebhooksLogo } from "react-icons/pi";
+import { SiMinutemailer, SiWebmoney, SiWebpack } from "react-icons/si";
+import { RiDoubleQuotesL } from "react-icons/ri";
+import { IoCheckmarkCircle } from "react-icons/io5";
+
+import team1 from "../assets/man1.jpg";
+import team2 from "../assets/man2.jpg";
+import team3 from "../assets/man3.jpg";
+import team4 from "../assets/man4.jpg";
+import team5 from "../assets/man5.jpg";
+import team6 from "../assets/man6.jpg";
+import team7 from "../assets/man7.jpg";
+
+export const services = [
+  {
+    icon: <FaCentos />,
+    head: "Recruitment and Staffing",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus iure illo architecto quidem molestiae.",
+  },
+  {
+    icon: <PiWebhooksLogo />,
+    head: "Payroll Management",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus iure illo architecto quidem molestiae.",
+  },
+  {
+    icon: <SiWebmoney />,
+    head: "Recruitment and Staffing",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus iure illo architecto quidem molestiae.",
+  },
+  {
+    icon: <SiWebpack />,
+    head: "Perfomance managment",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus iure illo architecto quidem molestiae.",
+  },
+  {
+    icon: <SiMinutemailer />,
+    head: "HR Consulting",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus iure illo architecto quidem molestiae.",
+  },
+  {
+    icon: <SiMinutemailer />,
+    head: "Email Marketing",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus iure illo architecto quidem molestiae.",
+  },
+];
+
+export const about_list = [
+  {
+    icon: <IoCheckmarkCircle />,
+    title: "Onboarding and conformation",
+  },
+  {
+    icon: <IoCheckmarkCircle />,
+    title: "Payroll managment",
+  },
+  {
+    icon: <IoCheckmarkCircle />,
+    title: "Leave managment",
+  },
+  {
+    icon: <IoCheckmarkCircle />,
+    title: "Disciplinary managment",
+  },
+  {
+    icon: <IoCheckmarkCircle />,
+    title: "Exit and Offboarding",
+  },
+  {
+    icon: <IoCheckmarkCircle />,
+    title: "Onboarding and conformation",
+  },
+  {
+    icon: <IoCheckmarkCircle />,
+    title: "Safety and health",
+  },
+];
+
+export const teams = [
+  {
+    name: "Some name",
+    role: "Manager",
+    img: team1,
+  },
+  {
+    name: "Some name",
+    role: "Manager",
+    img: team2,
+  },
+  {
+    name: "Some name",
+    role: "Manager",
+    img: team3,
+  },
+  {
+    name: "Some name",
+    role: "Manager",
+    img: team4,
+  },
+  {
+    name: "Some name",
+    role: "Manager",
+    img: team5,
+  },
+  {
+    name: "Some name",
+    role: "Manager",
+    img: team6,
+  },
+  {
+    name: "Some name",
+    role: "Manager",
+    img: team7,
+  },
+];
+
+export const feedback = [
+  {
+    image: team1,
+    icon: <RiDoubleQuotesL />,
+    name: "Joun Smith",
+    company: "Company",
+    comment:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus saepe officiis dolorum quaerat! Non saepe temporibus sequi autem est inventore.",
+  },
+  {
+    image: team2,
+    icon: <RiDoubleQuotesL />,
+    name: "Joun Smith",
+    company: "Company",
+    comment:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus saepe officiis dolorum quaerat! Non saepe temporibus sequi autem est inventore.",
+  },
+  {
+    image: team3,
+    icon: <RiDoubleQuotesL />,
+    name: "Joun Smith",
+    company: "Company",
+    comment:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus saepe officiis dolorum quaerat! Non saepe temporibus sequi autem est inventore.",
+  },
+];
