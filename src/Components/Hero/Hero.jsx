ce5a1fb0ec77473a3df787b4ef22b350b4f0cb32
boben195 <boben195@ukr.net>
2025-07-05 14:41:11 +0300

    add hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import "./Hero.css";
+import hero from "../../assets/hero.jpg";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <div className="hero">
+        <h1 className="hero_header">
+          Empower your Workforce{" "}
+          <span>
+            {" "}
+            Transforming <br />
+            Your Buisness
+          </span>{" "}
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Quasi
+          error numquam possimus atque harum quod.
+        </p>
+        <div className="hero_btn">
+          <button className="cta-btn">Get started</button>
+        </div>
+        <div className="icons"></div>
+        <img
+          className="hero_img"
+          src={hero}
+          alt="some groop of random people"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
