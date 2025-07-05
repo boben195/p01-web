9cacc368fcaa79700e7691c6c8f8d5f88b7199c0
boben195 <boben195@ukr.net>
2025-07-05 19:48:17 +0300

    add service

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./Services.css";
+import { services } from "../data";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div>
+      <div className="services" id="services">
+        <h2 className="servic_header">What We Offer</h2>
+        <p className="text">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae,
+          quasi?
+        </p>
+        <div className="service_container">
+          {services.map(({ icon, head, desc }) => {
+            return (
+              <div className="servise_list">
+                <div className="service_icon">
+                  <h2 className="icon">{icon}</h2>
+                </div>
+                <h2>{head}</h2>
+                <p>{desc}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
